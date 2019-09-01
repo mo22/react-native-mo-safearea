a5823f3ea5f8c1a46a2ec5cf75f77cb245c975f2
Moritz Moeller <mm@mxs.de>
2019-09-01 17:15:19 +0200

    ..

--- example/patchReactNavigationSafeAreaView.tsx
+++ example/patchReactNavigationSafeAreaView.tsx
@@ -6,6 +6,8 @@ import { View } from 'react-native';
 SafeAreaViewOrig.prototype.render = function render() {
   const props = this.props as SafeAreaViewOrigProps;
   const { forceInset, ...otherProps } = props;
+  console.log('XXX', forceInset);
+  // 09-01 17:13:44.860  4890  5559 I ReactNativeJS: 'XXX', { top: 'always', bottom: 'never', horizontal: 'always' }
   return (
     <SafeAreaView forceInsets={forceInset}>
       <View {...otherProps} />
